ead70cfcab940881c6cdb4a5fd7cdb9294815762
huynhkien <huynhhuukien2962@gmail.com>
2025-03-24 22:19:29 +0700

    Update backend

--- app.js
+++ app.js
@@ -11,16 +11,11 @@ const productRouter = require('./app/routes/product.route');
 
 const app = express();
 app.use(cookieParser());
-
-const allowedOrigins = [
-    'https://web-frontend-snowy-seven.vercel.app/',
-  ];
-
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: ['https://your-frontend-domain.vercel.app'],
+  origin: ['https://web-frontend-snowy-seven.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
